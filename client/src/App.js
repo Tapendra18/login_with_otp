84ef706fc1140d2fb516c658b4c87378b615d361
Tapendra18 <107476177+Tapendra18@users.noreply.github.com>
2023-01-29 19:34:00 +0530

    routing set in App js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from './pages/Dashboard';
+import Otp from "./pages/Otp";
+import Error from "./pages/Error";
+import Header from './components/Header';
+import { Route, Routes } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <>
-     
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/user/otp" element={<Otp />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
     </>
   );
 }
