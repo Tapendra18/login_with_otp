12efa617b2b9ca5adf14734e6fd03a06cd44e907
Tapendra18 <107476177+Tapendra18@users.noreply.github.com>
2023-01-29 19:25:59 +0530

    folder make

--- server/app.js
+++ server/app.js
@@ -3,3 +3,14 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const PORT = 4002;
+
+
+//middleware 
+
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(PORT, ()=>{
+    console.log(`server start at Port No :${PORT}`)
+})
